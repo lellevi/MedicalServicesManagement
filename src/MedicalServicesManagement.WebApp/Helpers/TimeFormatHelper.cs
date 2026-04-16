4007c3dd6acb44d84d9c1b60a7530c935ca3d1da
lellevi <liza.zemchonok@gmail.com>
2026-04-16 21:12:04 +0300

    fix time format helper

--- src/MedicalServicesManagement.WebApp/Helpers/TimeFormatHelper.cs
+++ src/MedicalServicesManagement.WebApp/Helpers/TimeFormatHelper.cs
@@ -4,14 +4,21 @@ namespace MedicalServicesManagement.WebApp.Helpers
 {
     public static class TimeFormatHelper
     {
-        public static string FormatDuration(TimeSpan duration)
+        public static string FormatDuration(DateTime start, DateTime end)
         {
+            var duration = end - start;
+
             if (duration.TotalHours >= 1)
             {
-                return $"{(int)duration.TotalHours}ч {(int)duration.Minutes:D2}мин";
+                return $"{(int)duration.TotalHours}ч {duration.Minutes:D2}мин";
             }
 
             return $"{duration.Minutes}мин";
+        }
+
+        public static string FormatTimeRange(DateTime start, DateTime end)
+        {
+            return $"{start:HH:mm}-{end:HH:mm}";
         }
     }
 }
